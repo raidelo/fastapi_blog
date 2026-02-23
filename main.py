ee766b4bd3dd645ef3d27350053ca566319df81a
raidelo <raidelosix@gmail.com>
2026-02-22 20:35:19 -0500

    feat: improve templates and add static files

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), "static")
 
 # { Start fake data section
 from faker import Faker  # noqa: E402
@@ -29,8 +31,8 @@ posts: list[dict] = [
 # } End fake data section
 
 
-@app.get("/posts", include_in_schema=False)
-@app.get("/", include_in_schema=False)
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request):
     return templates.TemplateResponse(
         request, "home.html", {"posts": posts, "title": "Home"}
